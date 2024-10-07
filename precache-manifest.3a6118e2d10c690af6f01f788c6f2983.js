8d09ad091bcc58ca3638c8cf8b800d939b7591d9
Shush25 <shushant.kumar.singh.5@gmail.com>
2024-10-07 21:39:42 +0530

    Updates

---
+++ precache-manifest.3a6118e2d10c690af6f01f788c6f2983.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35898b4318a8e11a81e2dcde82f197c2",
+    "url": "/scrapify/index.html"
+  },
+  {
+    "revision": "aacbe1ee6a0d2378cb45",
+    "url": "/scrapify/static/css/2.7b21c299.chunk.css"
+  },
+  {
+    "revision": "5d808f734601036ef69d",
+    "url": "/scrapify/static/css/main.9a2beca5.chunk.css"
+  },
+  {
+    "revision": "aacbe1ee6a0d2378cb45",
+    "url": "/scrapify/static/js/2.6191c964.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/scrapify/static/js/2.6191c964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200f85884ada37b38154",
+    "url": "/scrapify/static/js/3.bdc6a464.chunk.js"
+  },
+  {
+    "revision": "5d808f734601036ef69d",
+    "url": "/scrapify/static/js/main.6750b77e.chunk.js"
+  },
+  {
+    "revision": "5c794458528f591e98c9",
+    "url": "/scrapify/static/js/runtime-main.82bc29dd.js"
+  },
+  {
+    "revision": "6594d8392fafdf5447a87c8ae63df024",
+    "url": "/scrapify/static/media/about-bg.6594d839.png"
+  },
+  {
+    "revision": "c6862e0ca909b9a363a7bab1b908f92a",
+    "url": "/scrapify/static/media/community.c6862e0c.png"
+  },
+  {
+    "revision": "ffef44acacea89dd3323d592f9aa5d0f",
+    "url": "/scrapify/static/media/efficiency.ffef44ac.png"
+  },
+  {
+    "revision": "269cc7c9b0c8ed3f1e621078a983e1b2",
+    "url": "/scrapify/static/media/goal.269cc7c9.jpg"
+  },
+  {
+    "revision": "daa2a4fdd68c62caeb91836bc15f7abf",
+    "url": "/scrapify/static/media/home-bg.daa2a4fd.jpg"
+  },
+  {
+    "revision": "81b384fa7a37d443ec6dbfeb9ecddcdf",
+    "url": "/scrapify/static/media/innovations.81b384fa.png"
+  },
+  {
+    "revision": "1dd24af4e432fe5231aa3f706ef0438a",
+    "url": "/scrapify/static/media/insights.1dd24af4.png"
+  },
+  {
+    "revision": "a1d5940d4358036b4285f780c2f207b4",
+    "url": "/scrapify/static/media/logo.a1d5940d.png"
+  },
+  {
+    "revision": "c00741942d9cd93d7cba0b78c6727c1f",
+    "url": "/scrapify/static/media/monitoring_system.c0074194.png"
+  },
+  {
+    "revision": "81d5e19d1930ce3817bc9dd2fa303fe9",
+    "url": "/scrapify/static/media/savings.81d5e19d.png"
+  },
+  {
+    "revision": "4a803793ef0640107d87bf250de75bb2",
+    "url": "/scrapify/static/media/scrap_management.4a803793.png"
+  },
+  {
+    "revision": "64055459cc1535350261e5feb2b001d0",
+    "url": "/scrapify/static/media/vision.64055459.jpg"
+  }
+]);
